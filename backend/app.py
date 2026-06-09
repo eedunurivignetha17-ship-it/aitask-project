4f08b9c1bfeb7f3c47d7fa24ee849710dbefa6c8
bhavani <@bhavani>
2026-06-09 15:34:39 +0530

    Fixed backend AI route

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,25 @@
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-
-from router.ai import ai_bp
+from backend.router.ai import ai_bp
 
 app = Flask(
     __name__,
-    static_folder="../frontend",
+    static_folder="frontend",
     static_url_path=""
 )
 
 CORS(app)
 
+# REGISTER AI ROUTE
 app.register_blueprint(ai_bp)
 
+# HOME PAGE
 @app.route("/")
 def home():
     return send_from_directory(app.static_folder, "index.html")
 
+# SERVE FRONTEND FILES
 @app.route("/<path:path>")
 def serve_files(path):
     return send_from_directory(app.static_folder, path)
